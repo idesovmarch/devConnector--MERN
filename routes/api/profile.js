7619c17a115930980ddd0225639e0a6937573a2b
idesovmarch <bdhammer2288@gmail.com>
2019-01-27 02:40:44 -0500

    Start Profile Router

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,7 @@
 const express = require("express");
-
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
 
 //@route Get api/profile/test
 //@desc Tests profile route
