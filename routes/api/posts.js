531db3fe2057d06654f48871eb218613ff478353
idesovmarch <bdhammer2288@gmail.com>
2019-01-28 23:05:20 -0500

    Post functionality

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,10 +1,43 @@
 const express = require("express");
-
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+//Post Model
+const Post = require("../../models/Post");
+
+//Validation
+const validatePostInput = require("../../validation/post");
 
 //@route Get api/posts/test
 //@desc Tests post route
 //@access Public
 router.get("/test", (req, res) => res.json({ msg: "Posts Works" }));
+
+//@route POST api/posts
+//@desc Create post
+//@access Private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    //Check Validation
+    if (!isValid) {
+      //If any 400 errors, errors object
+      return res.status(400).json(errors);
+    }
+
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.name,
+      user: req.user.id
+    });
+
+    newPost.save().then(post => res.json(post));
+  }
+);
 
 module.exports = router;
